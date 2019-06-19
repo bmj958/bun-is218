5963c418a07aa86fdc7fa89bcb1d32dd7611a694
bmj958 <bmj958@github.com>
2019-06-18 23:24:47 -0400

    html class

--- index.php
+++ index.php
@@ -3,26 +3,27 @@ $fileName = 'Sacramentorealestatetransactions.csv';
 $program = new main($fileName);
 
 class main {
+    
     private $html
         
         public function _construct($fileName)
+        {
         
         $records = csv::getFileRecords($fileName);
         $table = html::table(records);
         system::printPage($table);
-    }
+        
+        }
 }
 
 class csv {
+    
     
     public static function getFileRecords($fileName){
         
         if (($handle = fopen($fileName, mode: "r")) !== FALSE) {
             while (($data = fgetcsv($handle, length: '1000', delimiter: ",")) !== FALSE) {
-                $num = count($data);
-                for ($c=0; $c < $num; $c++) {
-                    $records[] = $data;
-                }
+             $records[] = $data;
             }
             fclose($handle);
         }
@@ -30,3 +31,40 @@ class csv {
     }
     
 }
+
+class html {
+    
+    public static function tableRow($row) {
+        
+        $html = "<tr> $row </tr>";
+        
+        return $html;
+        
+    }
+    public static function table($records) {
+        //start table
+        $html = '<table>';
+        //header row
+        $html .= '<tr>';
+        $headings = array_shift( &array: $records);
+        foreach(headings as $key=>$value){
+            $html .= '<th>' . htmlspecialchars($key) . '</th>';
+        }
+        $html .= '</tr>';
+        
+        //data rows
+        foreach( $records as $key=>$value){
+            $html .= '<tr>';
+            foreach($value as $key2=>$value2){
+                $html .= '<td>' . htmlspecialchars($value2) . '</td>';
+            }
+            $html .= '</tr>';
+        }
+        
+        //finish table and return it
+        
+        $html .= '</table>';
+        return $html;
+    }
+}
+
